91725cc2aaa82f037a51af9679e3153f29532346
jaypatel_simform <jaypatel@simformsolutions.com>
2023-07-19 17:10:35 +0530

    feat: done counter feature using Input and Output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { CounterOutputComponent } from "./counter/counter-output/counter-output.component";
+import { CounterButtonComponent } from "./counter/counter-button/counter-button.component";
+import { CounterComponent } from "./counter/counter/counter.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CounterOutputComponent,
+        CounterButtonComponent,
+        CounterComponent,
+    ],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
